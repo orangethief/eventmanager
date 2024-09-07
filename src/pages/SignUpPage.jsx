6bf0401eb268cae50aefa99a8d39ffd70b253b6b
Lisa <helbig-lisa@web.de>
2024-09-07 16:33:47 +0200

    basic functions

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,12 +1,59 @@
 import Navbars from "../Pages/Navbar";
+import { baseUrl } from '../../config';
+import { useState } from 'react';
 
-const SinUpPage = () => {
+const SignUpPage = () => {
+
+  const signUp = async(event) => {
+    event.preventDefault();
+    console.log(email, password);
+
+    const response = await fetch(`${baseUrl}/api/users`, {
+      method: "POST",
+      body: JSON.stringify({ email: email, password:password }),
+      headers: {"Accept" : "application/json", "Content-Type" : "application/json"}
+    });
+    console.log(await response.json());
+  }
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <>
       <Navbars/>
-      <div>SinUpPage</div>
+      <div className="w-1/3 mx-auto">
+        <h1 className="text-3xl font-bold text-primary my-6">Register now!</h1>
+        <label className="input input-bordered flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="h-4 w-4 opacity-70">
+            <path
+              d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+            <path
+              d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+          </svg>
+          <input type="email" id="email" className="grow" placeholder="Email" onChange={(e) => setEmail(e.target.value)} value={email || ''}/>
+        </label>
+        <label className="input input-bordered flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="h-4 w-4 opacity-70">
+            <path
+              fillRule="evenodd"
+              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+              clipRule="evenodd" />
+          </svg>
+          <input type="password" id="password" className="grow" onChange={(e) => setPassword(e.target.value)} value={password || ''}  />
+        </label>
+        <button onClick={signUp} className="btn btn-accent">Sign Up</button>
+      </div>
     </>
   )
 }
 
-export default SinUpPage
+export default SignUpPage
