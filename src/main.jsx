5c83eceb7da7f8c5b828169a7d59f1a35f1f1098
Lisa <helbig-lisa@web.de>
2024-09-07 11:28:01 +0200

    2nd commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,41 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import EventDetailsPage from './pages/EventDetailsPage.jsx';
+import SignInPage from './pages/SignInPage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
+import CreateEventPage from './pages/CreateEventPage.jsx';
+import HomePage from './pages/HomePage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />
+  },
+  {
+    path: "/events/:id",
+    element: <EventDetailsPage />
+  },
+  {
+    path: "/login/",
+    element: <SignInPage />
+  },
+  {
+  path: "/signup/",
+  element: <SignUpPage />
+  },
+  {
+  path: "/newevent/",
+  element: <CreateEventPage />
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SinUpPage = () => {
+  return (
+    <div>SinUpPage</div>
+  )
+}
+
+export default SinUpPage
