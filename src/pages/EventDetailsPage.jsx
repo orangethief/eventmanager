49fa46f43809eb74a6d0dafae7c9f356be9acf1c
Lisa <helbig-lisa@web.de>
2024-09-14 13:44:49 +0200

    setup eventdetailpage

--- src/pages/EventDetailsPage.jsx
+++ src/pages/EventDetailsPage.jsx
@@ -2,23 +2,27 @@ import { useParams } from "react-router-dom";
 import Spinner from "../Spinner";
 import { useState, useEffect } from 'react';
 import { baseUrl } from '../../config';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 const EventDetailsPage = () => {
   const { id } = useParams();
 
-    const [event, setEvent] = useState(null);
+    const [event, setEvent] = useState({});
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-      const fetchEvent = async () => {
+      console.log(id)
+       const fetchEvent = async () => {
         try {
           const response = await fetch(`${baseUrl}/api/events/${id}`);
           if (!response.ok) {
             throw new Error('Network response was not ok');
           }
           const data = await response.json();
-          setEvent(data.results);
+          setEvent(data);
+          console.log(data)
         } catch (error) {
           setError(error);
         } finally {
@@ -26,8 +30,28 @@ const EventDetailsPage = () => {
         }
       };
 
-      fetchEvent();
+      fetchEvent() ;
     }, []);
+
+    useEffect(() => {
+      if (event.latitude && event.longitude) {
+      const map = L.map('map').setView([event.latitude, event.longitude], 16);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap contributors'
+
+      }).addTo(map);
+
+      L.marker([event.latitude, event.longitude]).addTo(map)
+        .bindPopup(event.title)
+        .openPopup();
+
+        return () => {
+          map.remove();
+        }
+      }
+    }, [event]);
 
     if (error) return <div>Error: {error.message}</div>;
 
@@ -41,21 +65,29 @@ const EventDetailsPage = () => {
 
   return (
     <>
-      <div key={id} className="card bg-base-100 shadow-xl">
-                    <div className="card-body">
-                      <h2 className="card-title">
-                        { events.title }
-                      </h2>
-                      <p>{event.description.substring(0, 90) + '...'}</p>
-                      <p>{formatEventDate(event)}</p>
-                      <p>{event.location}</p>
-                      <p>created on {formatCreatedDate(event)}</p>
-                      <div className="card-actions justify-between align-bottom">
-                        <Link to={`/events/${event.id}`} className="btn btn-primary">View</Link>
-                      </div>
-                    </div>
-                  </div>
+    <div className="max-w-screen m-6">
+
+    {loading ? (
+      <Spinner loading={loading} />
+    ) : (
+      <>
+      <div className="card bg-base-100 shadow-xl mx-8">
+        <div className="card-body">
+          <h1 className="text-2xl font-bold mb-6">{event.title}</h1>
+          <p>{event.description}</p>
+          <p>{formatEventDate(event)}</p>
+          <p>{event.location}</p>
+          <p><i>created on {formatCreatedDate(event)}</i></p>
+        </div>
+        <div id="map" className="h-96 w-1/3 mx-8" key={event.latitude + event.longitude}>
+
+        </div>
+      </div>
     </>
+     )}
+    </div>
+    </>
+
   )
 }
 
