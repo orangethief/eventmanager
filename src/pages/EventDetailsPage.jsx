2107e2ffc726be3ce618005ee1bdf3fa7e8474fe
Lisa <helbig-lisa@web.de>
2024-09-14 13:57:50 +0200

    adding back button

--- src/pages/EventDetailsPage.jsx
+++ src/pages/EventDetailsPage.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import Spinner from "../Spinner";
 import { useState, useEffect } from 'react';
 import { baseUrl } from '../../config';
@@ -65,21 +65,26 @@ const EventDetailsPage = () => {
 
   return (
     <>
-    <div className="max-w-screen m-6">
+    <div className="max-w-screen m-6 mx-8">
+    <Link to="/" className="btn btn-primary">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-chevron-left"><circle cx="12" cy="12" r="10"/><path d="m14 16-4-4 4-4"/></svg>
+    all events
+    </Link>
 
     {loading ? (
       <Spinner loading={loading} />
     ) : (
       <>
-      <div className="card bg-base-100 shadow-xl mx-8">
+      <div className="card bg-base-100 shadow-xl">
         <div className="card-body">
-          <h1 className="text-2xl font-bold mb-6">{event.title}</h1>
+          <h1 className="text-2xl font-bold">{event.title}</h1>
           <p>{event.description}</p>
           <p>{formatEventDate(event)}</p>
           <p>{event.location}</p>
           <p><i>created on {formatCreatedDate(event)}</i></p>
+          <div id="map" className="h-96 w-1/3" key={event.latitude + event.longitude}>
         </div>
-        <div id="map" className="h-96 w-1/3 mx-8" key={event.latitude + event.longitude}>
+
 
         </div>
       </div>
